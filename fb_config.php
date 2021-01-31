b5ea1d269d578b28388e51cd6089d21f17fca29d
Pog-Frog <60003686+Pog-Frog@users.noreply.github.com>
2021-01-31 04:49:53 +0200

    modified the fb_config, enter your own fb app_id, app_secret

--- fb_config.php
+++ fb_config.php
@@ -1,8 +1,8 @@
 <?php
     require_once('Facebook/autoload.php');
     $fb_obj = new \Facebook\Facebook([
-        'app_id' =>'401393217785660',
-        'app_secret' => 'aab7a3c1ce0d5828f548d1ab2e97d604',
+        'app_id' =>'insert your app id',
+        'app_secret' => 'insert your app secret key',
         'default_graph_version' => 'v2.10',
     ]);
     $handler = $fb_obj->getRedirectLoginHelper();
